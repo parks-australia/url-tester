2aee77dcb768409eff6079f0be9c271cadcbaa0d
Tim <timfletcher88@gmail.com>
2023-05-04 17:10:28 +1000

    Combined file call generation into one batch of scripts

--- file-list.js
+++ file-list.js
@@ -2,10 +2,20 @@
 
 // Get absolute files URLs from `/admin/content/files`, run this function in the console
 function getDrupalFiles() {
-  let list = '';
+  let list = [];
   jQuery('tbody .views-field-filename a').each(function() {
-    list += jQuery(this).attr('href') + '\n';
+    list.push(jQuery(this).attr('href').replace('/sites/default/files/', 'public://'));
   });
-  console.log(list);
-  copy(list);
+  return list;
 }
+
+const generateSqlQueries = (filesList) => {
+  let query='';
+  for (let i=0; i<filesList.length; i++) {
+    query += `CALL deleteFileRecord('${filesList[i]}');\n`;
+  }
+  console.log(query);
+  return query;
+}
+
+copy(generateSqlQueries(getDrupalFiles()));
